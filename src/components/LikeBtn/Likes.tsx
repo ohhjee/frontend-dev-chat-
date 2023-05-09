7e371b68926615805135650866ef36087e024578
ohhjee <orotunjigbega@gmail.com>
2023-05-09 22:58:38 +0100

    update

--- src/components/LikeBtn/Likes.tsx
+++ src/components/LikeBtn/Likes.tsx
@@ -61,7 +61,7 @@ export const Likes = () => {
       <div className="flex justify-between md:justify-center items-center w-full md:w-fit  rounded mt-[2rem] md:mt-0">
         <div className="flex md:flex-col md:h-[10vh] items-center bg-blue-500/20 px-1 rounded md:justify-center justify-between ">
           <div className="flex items-center md:flex-col">
-            <button aria-label="Name" id="al" className="mr-[.8rem] md:m-0">
+            <button aria-label="Name" id="idk" className="mr-[.8rem] md:m-0">
               <PlusIcon onClick={() => increase()} className="h-4 w-4" />
             </button>
             <div className="text-sm space-x-3">{count}</div>
